074bed5b4b5454e25d35c6ffb4fa8206b9af4911
ydokali <ydokkali@gmail.com>
2018-09-23 23:09:24 +0100

    google style program

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
+//  Copyright 2018 <dokali>
 #include<iostream>
 #include<vector>
 
-int main ()
-{
-
-   std::vector <int> myvect ={12,5,6,8,3};
-   for(int  var : myvect )
-   {
-     std::cout << var <<std::endl;
-   }
-   
-   std::cin.get();
-   return 0;
-
+int main() {
+  std::vector <int> myvect = { 12, 5, 96, 8, 3};
+  for (int var : myvect) {
+    std::cout << var <<std::endl; }
+  std::cin.get();
+  return 0;
 }
